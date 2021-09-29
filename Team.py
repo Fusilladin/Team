228e36aa7bacebe0a5b0b7a31da2574a5eef683b
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:53:41 -0500

    Add files via upload

---
+++ Team.py
@@ -0,0 +1,21 @@
+# TODO Create an empty list to maintain the player names
+players=[]
+
+
+# TODO Ask the user if they'd like to add players to the list..
+players=input("Would you like to add a player to the roster? Yes/No  ")
+if players.lower() == "yes":
+    print("Name")
+
+# IF the user answers "Yes", let them type in a name and add it to the list.
+# If the user answers "no", print out the team "roster"
+
+# TODO orint the number of players on the team
+
+# TODO print the player number and the play name
+# The player  number should start at the number one
+
+# TODO select a goalkeeper from the above roster
+
+# TODO print the goal keeper's name
+# Remember that lists use a zero based index
